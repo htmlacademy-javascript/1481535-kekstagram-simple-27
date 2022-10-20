a3a0507cc8a7f08ca5aa15b2243a243ebe810d69
Timofey Sapego <timon_sapego@mail.ru>
2022-10-20 10:49:13 +0300

    4 модуль

--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
 const PICTURES_COUNT = 25;
-const AVATARS_CAUNT = 6;
+const DESCRIPTION = 'Отдых летом на море. #египет #чилим';
 
 const likesCount = {
   MIN: 15,
   MAX: 200,
 };
 
-const commentLines = ['На пляже', 'Моя машина', 'Девушка моей мечты', 'На концерте хора', 'Мой закат'];
-
-const descriptions = [
-  'Отдых летом на море. #египет #чилим',
-  'Купил новый фотик! #new #saperfoto',
-  'Мой ужин сегодня #food # travel',
-  'Отдыхаем... #relax #photo',
-  'Берегите отношения со своими друзьями #friends #дружба',
-];
+const commentsCount = {
+  MIN: 0,
+  MAX: 200,
+};
 
 const getRandomNumber = (min, max) => {
   if (min < 0 || max < 0 || min === max) {
@@ -29,42 +24,23 @@ const getRandomNumber = (min, max) => {
 };
 
 const checkStringLength = (string, maxLength) => string.length <= maxLength;
+checkStringLength('string', 20);
 
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomNumber(0, elements.length - 1)];
-}
+let idCounter = 0;
+
+const createPicture = () => {
+  idCounter++;
+  return {
+    id: idCounter,
+    url: `photos/${idCounter}.jpg`,
+    description: DESCRIPTION,
+    likes: getRandomNumber(likesCount.MIN, likesCount.MAX),
+    comments: getRandomNumber(commentsCount.MIN, commentsCount.MAX),
+  };
+};
+
+const createPictures = () => Array.from({length: PICTURES_COUNT}, createPicture);
+
+createPictures();
 
 
-const createMessage = () =>
-  Array.from({
-      length: getRandomNumber(1, 2)
-    }, () =>
-    getRandomArrayElement(commentLines)
-  ).join(' ');
-
-const creatComment = (index) => ({
-  id: index,
-  avatar: `img/avatar-${getRandomNumber(1, AVATARS_CAUNT)}.svg`,
-  message: createMessage(),
-});
-
-const creatPicture = (index) => ({
-  id: index,
-  url: `photos/${index}.jpg`,
-  description: getRandomArrayElement(descriptions),
-  likes: getRandomNumber(likesCount.MIN, likesCount.MAX),
-  comments: Array.from({
-      length: getRandomNumber(0, 20)
-    },
-    (_item, commentIndex) => creatComment(commentIndex + 1)
-  ),
-});
-
-const getPictures = () =>
-  Array.from({
-      length: PICTURES_COUNT
-    }, (_, pictureIndex) =>
-    creatPicture(pictureIndex + 1)
-  );
-
-getPictures();
