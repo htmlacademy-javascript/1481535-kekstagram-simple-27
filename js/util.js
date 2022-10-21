ce6db4a528d076a64816f9226d36944a874f9fa5
Timofey Sapego <timon_sapego@mail.ru>
2022-10-21 13:44:19 +0300

    Разбил код на модули

---
+++ js/util.js
@@ -0,0 +1,14 @@
+// модуль со вспомогат. функциями
+const getRandomNumber = (min, max) => {
+  if (min < 0 || max < 0 || min === max) {
+    return NaN;
+  }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+getRandomNumber();
